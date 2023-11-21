a9a7cd7e1da24bd27896813ab29dce777054a015
Danielkojo <danielkojo005@gmail.com>
2023-11-21 17:19:01 +0000

    mobile auth

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,63 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import { auth } from '../firebase'
+import { Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { signOut } from 'firebase/auth';   
+
+
 
 const HomeScreen = () => {
+const navigation = useNavigation();
+
+
+    const logout = async () => {
+        try {
+          await signOut(auth);
+       navigation.replace("Login")
+        } catch (err) {
+          console.error(err);
+          // Set the error message in state
+          Alert.alert('Error', err.message);
+        }
+      };
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={styles.container}>
+      
+      <Text>Email:{auth.currentUser?.email}</Text>
+      <TouchableOpacity
+      onPress={logout}
+      style = {styles.button }
+      >
+        <Text style ={styles.buttonText}>Sign Out</Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
 export default HomeScreen
+ 
+const styles = StyleSheet. create({
+container:{
+flex:1,
+justifyContent: 'center',
+alignItems: 'center'
+}
+,
+ 
 
-const styles = StyleSheet.create({})
+button:{
+backgroundColor: "#0782f9",
+width: '60%',
+padding:15,
+borderRadius:10,
+alignItems: 'center',
+marginTop:40
+},
+buttonText:{
+color:'white',
+fontWeight:'700',
+fontSize:16
+},
+
+}) 
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,37 +1,72 @@
-import {KeyboardAvoidingView, StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import {KeyboardAvoidingView, Settings, StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View } from 'react-native'
+import React,{useState,useEffect} from 'react'
+import { auth } from '../firebase';
+import { createUserWithEmailAndPassword,signInWithEmailAndPassword } from 'firebase/auth';
+import { useNavigation } from '@react-navigation/native';
+import { Alert } from 'react-native';
 
 const LoginScreen = () => {
+const[email,setEmail]=useState('');
+const[password,setPassword]=useState('');
+
+const navigation = useNavigation()
+
+useEffect(()=>{
+    const unsubscribe = auth.onAuthStateChanged(user =>{
+        if(user){
+            navigation.replace("Home")
+        }
+    })
+    return unsubscribe
+},[])
+const signIn = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      // Redirect to the home screen after a successful login
+      console.log('signed in')// Replace "/home" with the actual route for your home screen.
+    } catch (err) {
+      console.error(err);
+      Alert.alert('Error', err.message);// Set the error message in state
+    }
+  };
+
+const SignUp = async ()=>{
+    try{await createUserWithEmailAndPassword(auth, email,password )}
+    catch(err){
+        console.error(err)
+        Alert.alert('Error', err.message);
+    }
+        };  
   return (
     <KeyboardAvoidingView style={styles.container}
     behavior='padding'>
     <View style={styles.inputContainer}>
         <TextInput placeholder='Email'
-        // value={}
-        // onChangeText={text =>}
+        value={email}
+        onChangeText={text =>setEmail(text)}
         style={styles.input}/>
     </View>
 
     <View style={styles.inputContainer}>
         <TextInput placeholder='password'
-        // value={}
-        // onChangeText={text =>}
+        value={password}
+        onChangeText={text =>setPassword(text)}
         style={styles.input}
         secureTextEntry/>
     </View>
     <View style={styles.buttonContainer}>
 <TouchableOpacity
-onPress={()=>{}
+onPress={signIn 
 
 }
 style={styles.button}
 >
-    <Text style={styles.button }>Login</Text>
+    <Text style={styles.buttonText }>Login</Text>
 </TouchableOpacity  >
 
 
 <TouchableOpacity
-onPress={()=>{}
+onPress={SignUp 
 
 }
 style={[styles.button, styles.buttonOutline ]}
@@ -57,7 +92,7 @@ const styles = StyleSheet.create({
     },
 
 input:{
-    backgroundColor:"white",
+    backgroundColor:'white',
     paddingHorizontal:15,
     paddingVertical:10,
     borderRadius:10,
@@ -70,9 +105,27 @@ input:{
         marginTop:40
     },
 button:{
-    backgroundColor: "0782f9"
+    backgroundColor: "#0782f9",
+    width: '100%',
+    padding:15,
+    borderRadius:10,
+    alignItems: 'center'
 },
-buttonOutline:{},
-buttonOutlineText:{},
-buttonText:{},
+buttonOutline:{
+    
+    backgroundColor:'white',
+    marginTop:5,
+    borderColor: "#0782f9",
+    borderWidth:2,
+},
+buttonOutlineText:{
+    color:'#0782f9',
+    fontWeight:'700',
+    fontSize:16
+},
+buttonText:{
+    color:'white',
+    fontWeight:'700',
+    fontSize:16
+},
 })
